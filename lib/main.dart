e90b553392a71549c7def8d80a3140b28ad2c597
bong <darksen7693@gmail.com>
2023-10-15 17:10:10 +0900

    bottom navigationbar 완료

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendPage(),
+          ChatPage(),
+          MorePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black26,
+        backgroundColor: Colors.grey[100],
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled), label: "profile"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble_2), label: "chat"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.ellipsis), label: "more"),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
