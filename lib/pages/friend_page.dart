3eb10575bbdd8333e0dad559d6726701dc75a089
bong <darksen7693@gmail.com>
2023-10-16 20:12:36 +0900

    채팅목록 보기 완료

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_app/components/my_profile.dart';
 import 'package:kakao_app/components/profile_card.dart';
-import 'package:kakao_app/model/user.dart';
+import 'package:kakao_app/models/user.dart';
 
 class FriendPage extends StatelessWidget {
   const FriendPage({super.key});
